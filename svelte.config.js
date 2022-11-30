83142c42056cf35f7685af8b20324cad62facd31
Mid <mrrmiddynight@gmail.com>
2022-12-01 00:37:12 +0100

    vercel changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-vercel";
 import preprocess from "svelte-preprocess";
 import relativeImages from "mdsvex-relative-images";
 import { mdsvex } from "mdsvex";
