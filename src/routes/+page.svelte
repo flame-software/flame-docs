5ecc49eeaa39af7f1c26340dbce5558d60bf7cc4
Mid <mrrmiddynight@gmail.com>
2022-12-03 17:06:36 +0100

    Fix social for homepage and add color tag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,53 @@
 <script>
 	import { browser } from "$app/environment";
+	import SvelteSeo from "svelte-seo/types/SvelteSeo";
 
 	if (browser) {
 		window.location = window.location + "v0.1/introduction";
 	}
 </script>
+
+<SvelteSeo
+	jsonLd={{
+		"@type": "WebSite",
+		headline: "Welcome to the Flame Documentation",
+		image: [import.meta.env.VITE_APP_URL + "/indexbanner.png"],
+		datePublished: new Date().toLocaleDateString(),
+		dateModified: new Date().toLocaleDateString(),
+		publisher: {
+			"@type": "Organization",
+			name: "Flame Software",
+			logo: {
+				"@type": "ImageObject",
+				url: import.meta.env.VITE_APP_URL + "/logo.png",
+			},
+		},
+	}}
+	twitter={{
+		site: "@FlameCommsApp",
+		title: "Welcome to the Flame Documentation",
+		description:
+			"Flame's Documentation website is the single place for all information about the Flame network and plans",
+		image: import.meta.env.VITE_APP_URL + "/indexbanner.png",
+		imageAlt: "Homepage image",
+	}}
+	openGraph={{
+		title: "Welcome to the Flame Documentation",
+		description:
+			"Flame's Documentation website is the single place for all information about the Flame network and plans",
+		url: import.meta.env.VITE_APP_URL,
+		type: "website",
+		images: [
+			{
+				url: import.meta.env.VITE_APP_URL + "/indexbanner.png",
+				alt: "Article thumbnail",
+			},
+		],
+	}}
+	title="Welcome to the Flame Documentation"
+	description="Flame's Documentation website is the single place for all information about the Flame network and plans"
+	keywords="Flame, Social, Network, Documentation, SaaS, Product, Plan, Vision, Freedom, Choice, Tech, Media"
+/>
 
 <svelte:head>
 	<title>Flame Docs</title>
